d7bf734b156308e787c2ad723f126f9cb8480492
Matthew Conlen <mc@mathisonian.com>
2016-10-07 19:29:03 -0700

    add image interface and fix args

--- browser.js
+++ browser.js
@@ -1,7 +1,18 @@
-
-module.exports = function (w, h) {
+var Canvas = function (w, h) {
   canvas = document.createElement('canvas');
-  canvas.width = w;
-  canvas.height = h;
+  if (w && h) {
+    canvas.width = w;
+    canvas.height = h;
+  } else if (w) {
+    canvas.width = w;
+    canvas.height = w;
+  }
   return canvas;
 }
+
+Canvas.prototype.Image = function () {
+  return (new Image).apply(null, arguments);
+}
+
+
+module.exports = Canvas;
